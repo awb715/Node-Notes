d3ffbb5c3b5d1bb5a142bd29cd22e2b63e908f63
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-26 21:44:11 -0700

    Linked Exports

--- app.js
+++ app.js
@@ -5,27 +5,30 @@ const _ = require('lodash');
 const yargs = require('yargs');
 //user create module
 const notes = require('./notes.js');
+//this connects exports in notes.js to this page.
 
 
 
 const argv=yargs.argv;
 var command = process.argv[2];
-console.log('Process ', process.argv);
+
 console.log('Yargs', argv);
 
 console.log('Command: ', command);
 if (command === 'add') {
-   console.log(notes.addNote);
+
     notes.addNote(argv.title,argv.body)
 }
 else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll();
 }
 else if (command === 'read') {
-    console.log('Reading note');
+  
+    notes.getNote(argv.title);
 }
 else if (command === 'remove') {
-    console.log('Removing note');
+   
+    notes.removeNote(argv.title);
 }
 else {
     console.log('Command not recognized');
--- notes.js
+++ notes.js
@@ -2,11 +2,27 @@ console.log('Starting Notes.js');
 
 
 
-var addNote = function (title, body){
+var addNote =  (title, body) => {
    console.log('Adding Note', title, body) ;
 };
 
+var getAll =  () => {
+   console.log('Getting all notes');
+    
+};
+
+var getNote = (title) => {
+    console.log('Getting ',title);
+}
+
+var removeNote = (title) => {
+    console.log('Removing ', title);
+}
+
 module.exports = {
     
-    addNote:addNote
+    addNote:addNote,
+    getAll:getAll,
+    getNote:getNote,
+    removeNote:removeNote
 };//entire object set to exports, accessing notes from another page will allow us to call these functions
