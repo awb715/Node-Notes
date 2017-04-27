6dcfda51c367353b43477471f5afd451b10c7b6b
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-26 20:52:45 -0700

    Cleaning demonstration code

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 
 
 console.log('Starting app.js');
-
+//3rd party module
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+//user create module
 const notes = require('./notes.js');
 
-
-//fetch contents from module and store in fs constant
-
-//var user = os.userInfo();
-//
-//fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//    if (err) {
-//        console.log('Unable to write to file');
-//   }
-//});
-
-
-//console.log(notes.addNote());
-
-//console.log(notes.add(2, 3));
-
-//console.log(_.isString(true));
-//console.log(_.isString('aaron'));
-var filteredArray = _.uniq(['Mike']);
-console.log(filteredArray);
