7fce15d344eb5958be1c6fcf049a37c6b4775bb9
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-26 22:51:14 -0700

    Parsing/String Object

--- Playground/json.js
+++ Playground/json.js
@@ -11,8 +11,30 @@
 
 
 //converts tring to original object
-var personString = '{"name":"Aaron","age":22}';
-var person = JSON.parse(personString);
+//var personString = '{"name":"Aaron","age":22}';
+//var person = JSON.parse(personString);
+//
+//console.log(typeof person);
+//console.log(person);
 
-console.log(typeof person);
-console.log(person);
+const fs = require('fs');
+
+var originalNote = {
+    
+    title:'some time',
+    body: 'some body'
+    
+};
+
+//original note string
+
+//converting object to string
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
