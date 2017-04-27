ea4d7f8b661ab6dbceff3845354d39c183d6e40b
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-26 21:33:31 -0700

    Running commands from terminal

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-
-
 console.log('Starting app.js');
 //3rd party module
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 //user create module
 const notes = require('./notes.js');
 
+
+
+const argv=yargs.argv;
+var command = process.argv[2];
+console.log('Process ', process.argv);
+console.log('Yargs', argv);
+
+console.log('Command: ', command);
+if (command === 'add') {
+   console.log(notes.addNote);
+    notes.addNote(argv.title,argv.body)
+}
+else if (command === 'list') {
+    console.log('Listing all notes');
+}
+else if (command === 'read') {
+    console.log('Reading note');
+}
+else if (command === 'remove') {
+    console.log('Removing note');
+}
+else {
+    console.log('Command not recognized');
+}
