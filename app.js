c31069b2ec46714c5cc4baa3f705208e52cc3fa6
Aaron Bell <aaronbellwebdev@gmail.com>
2017-04-28 22:27:47 -0700

    Ternary Operator use

--- app.js
+++ app.js
@@ -6,41 +6,35 @@ const yargs = require('yargs');
 //user create module
 const notes = require('./notes.js');
 //this connects exports in notes.js to this page.
-
-
-
-const argv=yargs.argv;
+const argv = yargs.argv;
 var command = argv._[0];
-
 console.log('Yargs', argv);
-
 console.log('Command: ', command);
 if (command === 'add') {
-
-   var note = notes.addNote(argv.title,argv.body);
-    
-    if(note){
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
         console.log('Note Created');
         console.log('--');
-         console.log('Title: ' +note.title);
-         console.log('--');
-         console.log('Body: ' +note.body);
+        console.log('Title: ' + note.title);
+        console.log('--');
+        console.log('Body: ' + note.body);
     }
-    else{
-         console.log('Title taken');
-       
+    else {
+        console.log('Title taken');
     }
 }
 else if (command === 'list') {
     notes.getAll();
 }
 else if (command === 'read') {
-  
     notes.getNote(argv.title);
 }
 else if (command === 'remove') {
-   
-    notes.removeNote(argv.title);
+   var noteRemoved= notes.removeNote(argv.title);
+    
+   var message = noteRemoved ? 'Note was removed': 'Note not found';
+    //first time using ternary operator. message will be left value if true and right value if noteRemoved false.
+    console.log(message);
 }
 else {
     console.log('Command not recognized');
